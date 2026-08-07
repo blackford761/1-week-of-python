2ddd0755c9082d2c7572998585105aa1e8908421
blackford761 <covenantjenmartin69@gmail.com>
2026-08-07 23:43:20 +0700

    adding loop and fixing variables

--- day3-4-project/to-do-list.py
+++ day3-4-project/to-do-list.py
@@ -1,19 +1,12 @@
 tasks = []
-print("To-Do List")
-print("1. Add a task")
-print("2. View tasks")
-print("3. Mark task as complete")
-print("4. Remove a task")
-print("5. Quit")
-action = input("Enter your action(1-5): ")
-if action == "1":
- tasks = input("Enter your task: ")
-elif action == "2":
- print("Your tasks are: ")
- print(tasks)
-elif action == "3":
- print(tasks)
-elif action == "4":
- print(tasks)
-elif action == "5":
- exit()
+action = ""
+while action != "5":
+    print("To-Do List")
+    print("1. Add a task")
+    print("2. View tasks")
+    print("3. Mark task as complete")
+    print("4. Remove a task")
+    print("5. Quit")
+    action = input("Enter your action(1-5): ")
+    if action == "1":
+        tasks = 
