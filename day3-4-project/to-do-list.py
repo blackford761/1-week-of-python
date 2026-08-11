234e11c0b989ce9615afb85e340fb8ffeb0d053b
blackford761 <covenantjenmartin69@gmail.com>
2026-08-11 22:02:49 +0700

    fix list and loop so it can appear after input again

--- day3-4-project/to-do-list.py
+++ day3-4-project/to-do-list.py
@@ -9,4 +9,16 @@ while action != "5":
     print("5. Quit")
     action = input("Enter your action(1-5): ")
     if action == "1":
-        tasks = 
+        more = "y"
+        while more == "y":
+            user_input = {}
+            user_input ["task"] = input("Enter your tasks: ")
+            user_input ["done"] = False
+            tasks.append(user_input)
+            more = input("do you want to add more tasks (y/n)?")
+    elif action == "2":
+        print(f"here is your list of tasks {tasks}")
+    elif action == "3":
+        remove = input(f"here is your list of tasks {tasks}, which one do you want to remove (type with number base on the order of the list you want to delete ex. 1, 3:)")
+        removed_tasks = tasks.pop(remove)
+        print (removed_tasks)
