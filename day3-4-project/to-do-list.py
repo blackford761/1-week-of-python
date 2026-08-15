1a59657bcc6d0de90cbfbd232b20fcbd33d3753b
blackford761 <covenantjenmartin69@gmail.com>
2026-08-15 22:58:24 +0700

    add mark as done menu using pop to divide done and ongoing tasks

--- day3-4-project/to-do-list.py
+++ day3-4-project/to-do-list.py
@@ -1,10 +1,13 @@
 tasks = []
 action = ""
+def repeat_view(tasks, status):
+    for i, item in enumerate(tasks):
+        print(f"{i+1}. {item['task']} - Done: {item['done']}")
 while action != "5":
     print("To-Do List")
     print("1. Add a task")
     print("2. View tasks")
-    print("3. Mark task as complete")
+    print("3. Mark as done")
     print("4. Remove a task")
     print("5. Quit")
     action = input("Enter your action(1-5): ")
@@ -17,9 +20,11 @@ while action != "5":
             tasks.append(user_input)
             more = input("do you want to add more tasks (y/n)?")
     elif action == "2":
-        for i, item in enumerate(tasks):
-            print(f"{i+1}. {item['task']} - Done {item['done']}")
+        repeat_view
     elif action == "3":
-        remove = input(f"here is your list of tasks {tasks}, which one do you want to remove (type with number base on the order of the list you want to delete ex. 1, 3:)")
-        removed_tasks = tasks.pop(remove)
-        print (removed_tasks)
+        repeat_view
+        remove = int(input(f"here is your list of tasks, which one do you want to Mark As Done (type with number base on the order of the list you want to delete ex. 1):"))
+        removed_tasks = tasks.pop(remove - 1)
+        print(f"here is your mark as done task/s {removed_tasks}")
+        repeat_view
+
